8144c2b58fefaf9493107908c8d395c0a1029270
mrunal-modi <mrunal.modi@gmail.com>
2023-04-13 12:20:16 +1000

    config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,6 @@
-console.log(process.env.REACT_APP_API_URL)
-export const API_URL = process.env.REACT_APP_API_URL;
+// console.log(process.env.REACT_APP_API_URL)
+// export const API_URL = process.env.REACT_APP_API_URL;
+
+export const API_URL = "http://localhost/api";
+console.log(API_URL)
+
