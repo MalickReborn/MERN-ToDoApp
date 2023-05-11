1420c216a257fea0b045c36b4006b961e192aba9
mrunal-modi <mrunal.modi@gmail.com>
2023-05-11 11:09:05 +1000

    index.html env-config.js inside public folder

---
+++ frontend/public/env-config.template.js
@@ -0,0 +1,1 @@
+window.REACT_APP_API_URL = "${REACT_APP_API_URL}";
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,8 @@
 // console.log(process.env.REACT_APP_API_URL)
 // export const API_URL = process.env.REACT_APP_API_URL;
 
-export const API_URL = "http://localhost/api";
+// export const API_URL = "http://localhost/api";
+
+const API_URL = window.REACT_APP_API_URL || process.env.REACT_APP_API_URL;
 console.log(API_URL)
 
