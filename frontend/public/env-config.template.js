c67726352969df287e33a247a39264495ad6d1ee
mrunal-modi <mrunal.modi@gmail.com>
2023-05-16 08:43:53 +1000

    orig

--- frontend/public/env-config.template.js
+++
@@ -1,1 +0,0 @@
-window.REACT_APP_API_URL = "${REACT_APP_API_URL}";
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,7 +2,5 @@
 // export const API_URL = process.env.REACT_APP_API_URL;
 
 export const API_URL = "http://localhost/api";
-
-// const API_URL = window.REACT_APP_API_URL || "http://localhost/api";
-console.log(API_URL) 
+console.log(API_URL)
 
