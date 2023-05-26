e738227f8a1fdf3feb31fbc5a1f0316063612877
mrunal-modi <mrunal.modi@gmail.com>
2023-05-26 16:05:35 +1000

    changed frontend config env

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 // console.log(process.env.REACT_APP_API_URL)
 // export const API_URL = process.env.REACT_APP_API_URL;
 
-export const API_URL = "http://localhost/api";
+export const API_URL = "http://demo.com/api";
 console.log(API_URL)
 
